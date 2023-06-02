201127cc85ee4a5b1bcb692d2d792d1d369c9090
Berke Lahna <120329487+BerkeLahna@users.noreply.github.com>
2023-06-02 17:37:48 +0300

    Update omegatest.py

--- omegatest.py
+++ omegatest.py
@@ -9,7 +9,7 @@ def main():
         'age': st.slider("Enter age:", min_value=18, max_value=100, value=30),
         'job': st.text_input("Enter job:"),
         'marital': st.selectbox("Enter marital status:", ['single', 'married', 'divorced']),
-        'education': st.selectbox("Enter education level:", ['primary', 'secondary', 'tertiary']),
+        'education': st.selectbox("Enter education level:", ['basic.4y','basic.6y','basic.9y', 'high.school', 'university.degree','professional.course','illiterate']),
         'default': st.selectbox("Has credit in default:", ['no', 'yes']),
         'housing': st.selectbox("Has a housing loan:", ['no', 'yes']),
         'loan': st.selectbox("Has a personal loan:", ['no', 'yes']),
@@ -22,10 +22,10 @@ def main():
         'previous': st.slider("Enter number of contacts performed before this campaign for this client:", min_value=0, max_value=50, value=0),
         'poutcome': st.selectbox("Outcome of the previous marketing campaign:", ['failure', 'success', 'nonexistent']),
         'emp.var.rate': st.slider("Enter employment variation rate:", min_value=-3.0, max_value=3.0, value=0.0, step=0.1),
-        'cons.price.idx': st.slider("Enter consumer price index:", min_value=85.0, max_value=105.0, value=93.0, step=0.1),
-        'cons.conf.idx': st.slider("Enter consumer confidence index:", min_value=-50.0, max_value=-10.0, value=-30.0, step=0.1),
+        'cons.price.idx': st.slider("Enter consumer price index:", min_value=0, max_value=105.0, value=93.0, step=0.1),
+        'cons.conf.idx': st.slider("Enter consumer confidence index:", min_value=-0, max_value=-10.0, value=-30.0, step=0.1),
         'euribor3m': st.slider("Enter euribor 3 month rate:", min_value=0.0, max_value=5.0, value=3.0, step=0.01),
-        'nr.employed': st.slider("Enter number of employees:", min_value=4000.0, max_value=10000.0, value=5000.0, step=10.0),
+        'nr.employed': st.slider("Enter number of employees:", min_value=0, max_value=10000.0, value=5000.0, step=10.0),
     }
     
 
