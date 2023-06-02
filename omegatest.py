2f28ce1ad74b8a8d7438f92f17637d7f46f53b60
Berke Lahna <120329487+BerkeLahna@users.noreply.github.com>
2023-06-02 18:08:59 +0300

    Update omegatest.py

--- omegatest.py
+++ omegatest.py
@@ -25,7 +25,7 @@ def main():
         'cons.price.idx': st.slider("Enter consumer price index:", min_value=0.0, max_value=105.0, value=93.0, step=0.1),
         'cons.conf.idx': st.slider("Enter consumer confidence index:", min_value=-50.0, max_value=-10.0, value=-30.0, step=0.1),    
         'euribor3m': st.slider("Enter euribor 3 month rate:", min_value=0.0, max_value=5.0, value=3.0, step=0.01),
-        'nr.employed': st.slider("Enter number of employees:", min_value=0, max_value=10000.0, value=5000.0, step=10.0),
+        'nr.employed': st.slider("Enter number of employees:", min_value=0.0, max_value=10000.0, value=5000.0, step=10.0),
     }
     
 
