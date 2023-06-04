803d4be573793f509128dabe24f15f3d049dfee2
Berke Lahna <120329487+BerkeLahna@users.noreply.github.com>
2023-06-04 13:43:48 +0300

    Update testt.py

--- testt.py
+++ testt.py
@@ -2,16 +2,17 @@ import streamlit as st
 
 st.title("Example")
 
+if "age" not in st.session_state:
+    st.session_state.age = 30
+
 col1, col2 = st.columns([3, 1])
 
 with col1:
-    age = st.slider("Enter age:", min_value=18, max_value=100, value=30, key="age_slider")
+    age = st.slider("Enter age:", min_value=18, max_value=100, value=st.session_state.age)
 
 with col2:
-    age_input = st.number_input(" ", min_value=18, max_value=100, value=30, key="age_input")
+    age_input = st.number_input(" ", min_value=18, max_value=100, value=st.session_state.age)
 
-if age != age_input:
-    st.session_state.age_slider = age_input
-    st.session_state.age_input = age
+st.session_state.age = age_input
 
 st.write(f"Age: {age}")
