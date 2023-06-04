84154ebe441899fc23465039ec3f6e65c9fe6170
Berke Lahna <120329487+BerkeLahna@users.noreply.github.com>
2023-06-04 14:41:47 +0300

    Update testt.py

--- testt.py
+++ testt.py
@@ -9,11 +9,18 @@ def plus_one():
         pass
     return
 
+
+def plus_one():
+    if st.session_state["slider"] < 10:
+        st.session_state.slider -= 1
+    else:
+        pass
+    return
 # when creating the button, assign the name of your callback
 # function to the on_click parameter
 with col2:
     add_one = st.button("Add one to the slider", on_click=plus_one, key="add_one")
-
+    remove_one = st.button("remove one from the slider", on_click=minus_one, key="add_one")
 # create the slider
 with col1:
     slide_val = st.slider("Pick a number", 0, 10, key="slider")
