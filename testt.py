fc09c5d067aea9f49d93217db43ec66d8791dd34
Berke Lahna <120329487+BerkeLahna@users.noreply.github.com>
2023-06-04 13:40:44 +0300

    Update testt.py

--- testt.py
+++ testt.py
@@ -8,6 +8,6 @@ with col1:
     age = st.slider("Enter age:", min_value=18, max_value=100, value=30)
 
 with col2:
-    age_input = st.number_input("Age:", min_value=18, max_value=100, value=30)
+    age_input = st.number_input( min_value=18, max_value=100, value=30)
 
 st.write(f"Age: {age}")
