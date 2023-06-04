434bbc724d0d526590b40f10ed4148da42ba4ca7
Berke Lahna <120329487+BerkeLahna@users.noreply.github.com>
2023-06-04 14:01:29 +0300

    Update testt.py

--- testt.py
+++ testt.py
@@ -66,31 +66,31 @@ def prepare_input_data(input_data):
 
 if name == "main":
 # Initialize session_state variables
-st.session_state.slider = 0
-st.session_state.numeric = 0
+    st.session_state.slider = 0
+    st.session_state.numeric = 0
 # Display the second code
-st.title("Example")
+    st.title("Example")
 
-def update_slider():
-    st.session_state.slider = st.session_state.numeric
+    def update_slider():
+        st.session_state.slider = st.session_state.numeric
 
-def update_numin():
-    st.session_state.numeric = st.session_state.slider
+    def update_numin():
+        st.session_state.numeric = st.session_state.slider
 
-col1, col2 = st.columns([3, 1])
+    col1, col2 = st.columns([3, 1])
 
-with col1:
-    slider_value = st.slider('slider', min_value=0,
+    with col1:
+        slider_value = st.slider('slider', min_value=0,
                              value=0,
                              max_value=5,
                              step=1,
                              key='slider', on_change=update_numin)
 
-with col2:
-    val = st.number_input('Input', value=0, key='numeric', on_change=update_slider)
+    with col2:
+        val = st.number_input('Input', value=0, key='numeric', on_change=update_slider)
 
 # Run the main application
-main()
+    main()
 
 
 
