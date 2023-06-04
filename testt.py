fbd9a5c512fa6288f6af44488dd29dfbc0f6c661
Berke Lahna <120329487+BerkeLahna@users.noreply.github.com>
2023-06-04 14:59:10 +0300

    Update testt.py

--- testt.py
+++ testt.py
@@ -1,7 +1,7 @@
 import streamlit as st# slider value up to 10
 
 col1, col2 = st.columns([3,1])
-container = st.container()
+
 def plus_one():
     if st.session_state["slider"] < 1000:
         st.session_state.slider += 1
@@ -19,8 +19,8 @@ def minus_one():
 # when creating the button, assign the name of your callback
 # function to the on_click parameter
 with col2:
-    add_one = container.st.button("+1", on_click=plus_one, key="add_one")
-    remove_one = container.st.button("-1", on_click=minus_one, key="minus_one")
+    add_one = st.button("+1", on_click=plus_one, key="add_one")
+    remove_one = st.button("-1", on_click=minus_one, key="minus_one")
 # create the slider
 with col1:
     slide_val = st.slider("Pick a number", -1000, 1000, key="slider")
