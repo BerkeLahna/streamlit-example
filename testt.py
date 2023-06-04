7c863ad9f1e04c2cc4ce528de4b3e8b0f0e435be
Berke Lahna <120329487+BerkeLahna@users.noreply.github.com>
2023-06-04 15:11:19 +0300

    Update testt.py

--- testt.py
+++ testt.py
@@ -1,35 +1,31 @@
-import streamlit as st# slider value up to 10
+import streamlit as st
 
-col1, col2,col3,col4 = st.columns([6,1,1,1])
+col1, col2, col3, col4 = st.columns([6, 0.5, 0.5, 0.5])
+
+# Initialize the session state slider value if it doesn't exist
+if "slider" not in st.session_state:
+    st.session_state.slider = 0
 
 def plus(value):
-    if st.session_state["slider"] < 1000:
+    if st.session_state.slider < 1000:
         st.session_state.slider += value
-    else:
-        pass
-    return
-
 
 def minus(value):
-    if st.session_state["slider"] > -1000:
+    if st.session_state.slider > -1000:
         st.session_state.slider -= value
-    else:
-        pass
-    return
-# when creating the button, assign the name of your callback
-# function to the on_click parameter
 
-
-with col2:
-    add_one = st.button("+1", on_click=plus(1))
-    remove_one = st.button("-1", on_click=minus(1))
-with col3:
-    add_five = st.button("+5", on_click=plus(5))
-    remove_five = st.button("-1", on_click=minus(5))
-    
-with col4:
-    add_ten = st.button("+10", on_click=plus(10))
-    remove_ten = st.button("-1", on_click=minus(10))
-# create the slider
+# Create the slider
 with col1:
     slide_val = st.slider("Pick a number", -1000, 1000, key="slider")
+
+with col2:
+    add_one = st.button("+1", on_click=plus, args=(1,))
+    remove_one = st.button("-1", on_click=minus, args=(1,))
+
+with col3:
+    add_five = st.button("+5", on_click=plus, args=(5,))
+    remove_five = st.button("-5", on_click=minus, args=(5,))
+
+with col4:
+    add_ten = st.button("+10", on_click=plus, args=(10,))
+    remove_ten = st.button("-10", on_click=minus, args=(10,))
