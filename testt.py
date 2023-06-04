506725170edaa57ebe3c37e6b3510856b643155b
Berke Lahna <120329487+BerkeLahna@users.noreply.github.com>
2023-06-04 14:35:52 +0300

    Update testt.py

--- testt.py
+++ testt.py
@@ -1,27 +1,14 @@
-import streamlit as st
+# slider value up to 10
+def plus_one():
+    if st.session_state["slider"] < 10:
+        st.session_state.slider += 1
+    else:
+        pass
+    return
 
-st.title("Example")
+# when creating the button, assign the name of your callback
+# function to the on_click parameter
+add_one = st.button("Add one to the slider", on_click=plus_one, key="add_one")
 
-def update_slider():
-    st.session_state.slider = st.session_state.numeric
-
-def update_numin():
-    st.session_state.numeric = st.session_state.slider
-
-col1, col2 = st.columns([3, 1])
-
-with col1:
-    slider_value = st.slider('slider', min_value=0,
-                             value=0,
-                             max_value=5,
-                             step=1,
-                             key='slider', on_change=update_numin)
-
-with col2:
-    if st.button("Increment"):
-        st.session_state.slider += st.number_input.value
-
-    st.number_input('Input', value=0, key='numeric', on_change=update_slider)
-
-    if st.button("Decrement"):
-        st.session_state.slider -= st.number_input.value
+# create the slider
+slide_val = st.slider("Pick a number", 0, 10, key="slider")
