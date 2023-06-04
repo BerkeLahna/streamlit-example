365b604413ea15434e8a430b56c9c222869fbddc
Berke Lahna <120329487+BerkeLahna@users.noreply.github.com>
2023-06-04 21:04:39 +0300

    Update omegatest.py

--- omegatest.py
+++ omegatest.py
@@ -6,7 +6,7 @@ def main():
     
     # Accept inputs from the user
     input_data = {
-        'age': st.slider("Enter age:", min_value=18, max_value=100, value=30),
+        'age': st.number_input("Select a number", min_value=18, max_value=100, value=50, step=1),
         'job':  st.selectbox("Enter job:", ['blue-collar','services','admin.', 'entrepreneur', 'self-employed','technician','management','student','retired','housemaid','unemployed']),
         'marital': st.selectbox("Enter marital status:", ['single', 'married', 'divorced']),
         'education': st.selectbox("Enter education level:", ['basic.4y','basic.6y','basic.9y', 'high.school', 'university.degree','professional.course','illiterate']),
@@ -16,16 +16,16 @@ def main():
         'contact': st.selectbox("Preferred contact type:", ['cellular', 'telephone']),
         'month': st.selectbox("Last contact month:", ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']),
         'day_of_week': st.selectbox("Last contact day of the week:", ['mon', 'tue', 'wed', 'thu', 'fri']),
-        'duration': st.slider("Enter call duration in seconds:", min_value=0, max_value=3600, value=300),
-        'campaign': st.slider("Enter number of contacts performed during this campaign for this client:", min_value=0, max_value=50, value=5),
-        'pdays': st.slider("Enter number of days that passed after the client was last contacted from a previous campaign:", min_value=0, max_value=1000, value=0),
-        'previous': st.slider("Enter number of contacts performed before this campaign for this client:", min_value=0, max_value=50, value=0),
+        'duration': st.number_input("Enter call duration in seconds:", min_value=0, max_value=3600, value=300,step = 1),
+        'campaign': st.number_input("Enter number of contacts performed during this campaign for this client:", min_value=0, max_value=50, value=5,step = 1),
+        'pdays': st.number_input("Enter number of days that passed after the client was last contacted from a previous campaign:", min_value=0, max_value=1000, value=0,step = 1),
+        'previous': st.number_input("Enter number of contacts performed before this campaign for this client:", min_value=0, max_value=50, value=0,step = 1),
         'poutcome': st.selectbox("Outcome of the previous marketing campaign:", ['failure', 'success', 'nonexistent']),
-        'emp.var.rate': st.slider("Enter employment variation rate:", min_value=-5.0, max_value=5.0, value=0.0, step=0.1),
-        'cons.price.idx': st.slider("Enter consumer price index:", min_value=0.0, max_value=105.0, value=93.0, step=0.1),
-        'cons.conf.idx': st.slider("Enter consumer confidence index:", min_value=-50.0, max_value=100.0, value=0.0, step=0.1),    
-        'euribor3m': st.slider("Enter euribor 3 month rate:", min_value=0.0, max_value=5.0, value=3.0, step=0.01),
-        'nr.employed': st.slider("Enter number of employees:", min_value=0.0, max_value=10000.0, value=5000.0, step=10.0),
+        'emp.var.rate': st.number_input("Enter employment variation rate:", min_value=-5.0, max_value=5.0, value=0.0, step=0.1),
+        'cons.price.idx': st.number_input("Enter consumer price index:", min_value=0.0, max_value=105.0, value=93.0, step=0.1),
+        'cons.conf.idx': st.number_input("Enter consumer confidence index:", min_value=-50.0, max_value=100.0, value=0.0, step=0.1),    
+        'euribor3m': st.number_input("Enter euribor 3 month rate:", min_value=0.0, max_value=5.0, value=3.0, step=0.01),
+        'nr.employed': st.number_input("Enter number of employees:", min_value=0.0, max_value=10000.0, value=5000.0, step=10.0),
     }
     
 
@@ -38,7 +38,7 @@ def main():
 
 def perform_classification(input_data):
     # Load the trained model
-    model = joblib.load("bank_model (1).pkl")
+    model = joblib.load("bank_model.pkl")
     
     # Prepare the input data for prediction
     X = prepare_input_data(input_data)
