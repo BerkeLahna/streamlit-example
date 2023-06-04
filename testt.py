bd43af38038447d39374247e2504edcbc792ec58
Berke Lahna <120329487+BerkeLahna@users.noreply.github.com>
2023-06-04 14:39:39 +0300

    Update testt.py

--- testt.py
+++ testt.py
@@ -11,7 +11,7 @@ def plus_one():
 
 # when creating the button, assign the name of your callback
 # function to the on_click parameter
-add_one = col1.st.button("Add one to the slider", on_click=plus_one, key="add_one")
+add_one = col2.st.button("Add one to the slider", on_click=plus_one, key="add_one")
 
 # create the slider
-slide_val = col2.st.slider("Pick a number", 0, 10, key="slider")
+slide_val = col1.st.slider("Pick a number", 0, 10, key="slider")
