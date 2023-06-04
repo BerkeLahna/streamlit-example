55edbcf7b2d4b8ec6d10ac09aa3141f916de1807
Berke Lahna <120329487+BerkeLahna@users.noreply.github.com>
2023-06-04 13:30:04 +0300

    Update testt.py

--- testt.py
+++ testt.py
@@ -3,51 +3,61 @@ import joblib
 
 def main():
     st.title("Bank Information Classification")
-
+    
     # Accept inputs from the user
     input_data = {
         'age': st.slider("Enter age:", min_value=18, max_value=100, value=30),
-        'job': st.selectbox("Enter job:", ['blue-collar', 'services', 'admin.', 'entrepreneur', 'self-employed',
-                                          'technician', 'management', 'student', 'retired', 'housemaid', 'unemployed']),
+        'job':  st.selectbox("Enter job:", ['blue-collar', 'services', 'admin.', 'entrepreneur', 'self-employed', 'technician', 'management', 'student', 'retired', 'housemaid', 'unemployed']),
         'marital': st.selectbox("Enter marital status:", ['single', 'married', 'divorced']),
-        'education': st.selectbox("Enter education level:",
-                                  ['basic.4y', 'basic.6y', 'basic.9y', 'high.school', 'university.degree',
-                                   'professional.course', 'illiterate']),
+        'education': st.selectbox("Enter education level:", ['basic.4y', 'basic.6y', 'basic.9y', 'high.school', 'university.degree', 'professional.course', 'illiterate']),
         'default': st.selectbox("Has credit in default:", ['no', 'yes']),
         'housing': st.selectbox("Has a housing loan:", ['no', 'yes']),
         'loan': st.selectbox("Has a personal loan:", ['no', 'yes']),
         'contact': st.selectbox("Preferred contact type:", ['cellular', 'telephone']),
-        'month': st.selectbox("Last contact month:",
-                              ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']),
+        'month': st.selectbox("Last contact month:", ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']),
         'day_of_week': st.selectbox("Last contact day of the week:", ['mon', 'tue', 'wed', 'thu', 'fri']),
         'duration': st.slider("Enter call duration in seconds:", min_value=0, max_value=3600, value=300),
-        'campaign': st.slider("Enter number of contacts performed during this campaign for this client:",
-                              min_value=0, max_value=50, value=5),
-        'pdays': st.slider("Enter number of days that passed after the client was last contacted from a previous campaign:",
-                           min_value=0, max_value=1000, value=0),
-        'previous': st.slider("Enter number of contacts performed before this campaign for this client:",
-                              min_value=0, max_value=50, value=0),
+        'campaign': st.slider("Enter number of contacts performed during this campaign for this client:", min_value=0, max_value=50, value=5),
+        'pdays': st.slider("Enter number of days that passed after the client was last contacted from a previous campaign:", min_value=0, max_value=1000, value=0),
+        'previous': st.slider("Enter number of contacts performed before this campaign for this client:", min_value=0, max_value=50, value=0),
         'poutcome': st.selectbox("Outcome of the previous marketing campaign:", ['failure', 'success', 'nonexistent']),
         'emp.var.rate': st.slider("Enter employment variation rate:", min_value=-3.0, max_value=3.0, value=0.0, step=0.1),
         'cons.price.idx': st.slider("Enter consumer price index:", min_value=0.0, max_value=105.0, value=93.0, step=0.1),
-        'cons.conf.idx': st.slider("Enter consumer confidence index:", min_value=-50.0, max_value=100.0, value=0.0,
-                                   step=0.1),
+        'cons.conf.idx': st.slider("Enter consumer confidence index:", min_value=-50.0, max_value=100.0, value=0.0, step=0.1),    
         'euribor3m': st.slider("Enter euribor 3 month rate:", min_value=0.0, max_value=5.0, value=3.0, step=0.01),
         'nr.employed': st.slider("Enter number of employees:", min_value=0.0, max_value=10000.0, value=5000.0, step=10.0),
     }
 
-    # Add input boxes for incrementing or decrementing the values
-    st.subheader("Increment/Decrement Values")
-    increment_decrement = {
-        'age': st.number_input("Age", value=input_data['age']),
-        'duration': st.number_input("Call Duration (seconds)", value=input_data['duration']),
-        'campaign': st.number_input("Number of Contacts during this Campaign", value=input_data['campaign']),
-        'pdays': st.number_input("Days since Last Contact", value=input_data['pdays']),
-        'previous': st.number_input("Number of Contacts before this Campaign", value=input_data['previous']),
-    }
+    # Create a container with two columns
+    col1, col2 = st.beta_columns([3, 1])
 
-    # Update the input_data dictionary with the incremented/decremented values
-    input_data.update(increment_decrement)
+    # Add sliders to the first column
+    with col1:
+        st.subheader("Sliders")
+        input_data['age'] = st.slider("Enter age:", min_value=18, max_value=100, value=30)
+        input_data['duration'] = st.slider("Enter call duration in seconds:", min_value=0, max_value=3600, value=300)
+        input_data['campaign'] = st.slider("Enter number of contacts performed during this campaign for this client:", min_value=0, max_value=50, value=5)
+        input_data['pdays'] = st.slider("Enter number of days that passed after the client was last contacted from a previous campaign:", min_value=0, max_value=1000, value=0)
+        input_data['previous'] = st.slider("Enter number of contacts performed before this campaign for this client:", min_value=0, max_value=50, value=0)
+        input_data['emp.var.rate'] = st.slider("Enter employment variation rate:", min_value=-3.0, max_value=3.0, value=0.0, step=0.1)
+        input_data['cons.price.idx'] = st.slider("Enter consumer price index:", min_value=0.0, max_value=105.0, value=93.0, step=0.1)
+        input_data['cons.conf.idx'] = st.slider("Enter consumer confidence index:", min_value=-50.0, max_value=100.0, value=0.0, step=0.1)
+        input_data['euribor3m'] = st.slider("Enter euribor 3 month rate:", min_value=0.0, max_value=5.0, value=3.0, step=0.01)
+        input_data['nr.employed'] = st.slider("Enter number of employees:", min_value=0.0, max_value=10000.0, value=5000.0, step=10.0)
+
+    # Add number input boxes to the second column
+    with col2:
+        st.subheader("Number Input")
+        input_data['age'] = st.number_input("Enter age:", min_value=18, max_value=100, value=30)
+        input_data['duration'] = st.number_input("Enter call duration in seconds:", min_value=0, max_value=3600, value=300)
+        input_data['campaign'] = st.number_input("Enter number of contacts performed during this campaign for this client:", min_value=0, max_value=50, value=5)
+        input_data['pdays'] = st.number_input("Enter number of days that passed after the client was last contacted from a previous campaign:", min_value=0, max_value=1000, value=0)
+        input_data['previous'] = st.number_input("Enter number of contacts performed before this campaign for this client:", min_value=0, max_value=50, value=0)
+        input_data['emp.var.rate'] = st.number_input("Enter employment variation rate:", min_value=-3.0, max_value=3.0, value=0.0, step=0.1)
+        input_data['cons.price.idx'] = st.number_input("Enter consumer price index:", min_value=0.0, max_value=105.0, value=93.0, step=0.1)
+        input_data['cons.conf.idx'] = st.number_input("Enter consumer confidence index:", min_value=-50.0, max_value=100.0, value=0.0, step=0.1)
+        input_data['euribor3m'] = st.number_input("Enter euribor 3 month rate:", min_value=0.0, max_value=5.0, value=3.0, step=0.01)
+        input_data['nr.employed'] = st.number_input("Enter number of employees:", min_value=0.0, max_value=10000.0, value=5000.0, step=10.0)
 
     # Add a button to perform the classification
     if st.button("Perform Classification"):
