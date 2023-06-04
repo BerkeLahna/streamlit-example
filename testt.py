fbbb3d357dd74ac7e1b00b80f6373f1a00e7636a
Berke Lahna <120329487+BerkeLahna@users.noreply.github.com>
2023-06-04 13:42:16 +0300

    Update testt.py

--- testt.py
+++ testt.py
@@ -5,9 +5,13 @@ st.title("Example")
 col1, col2 = st.columns([3, 1])
 
 with col1:
-    age = st.slider("Enter age:", min_value=18, max_value=100, value=30)
+    age = st.slider("Enter age:", min_value=18, max_value=100, value=30, key="age_slider")
 
 with col2:
-    age_input = st.number_input(" ", min_value=18, max_value=100, value=30)
+    age_input = st.number_input(" ", min_value=18, max_value=100, value=30, key="age_input")
+
+if age != age_input:
+    st.session_state.age_slider = age_input
+    st.session_state.age_input = age
 
 st.write(f"Age: {age}")
